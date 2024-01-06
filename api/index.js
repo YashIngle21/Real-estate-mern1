f35888972b85d766316d7b9bbfe986c94fdbfe67
YashIngle21 <yashingle1729@gmail.com>
2024-01-06 18:36:10 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 const express = require("express")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+    .then (()=>{
+        console.log("COnnected to mongodb");
+    }).catch((err)=>{
+        console.log(err);
+    });
+
 
 const app = express();
 
