2bc045ff79794b1947947d8757ebb3dd6eba2b87
YashIngle21 <yashingle1729@gmail.com>
2024-01-08 01:01:04 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 import bcrpyt from "bcrypt"
+import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-
-const signup = async (req,res,next)=>{
+export const signup = async (req,res,next)=>{
     const { username, email, password} = req.body
     const hashedPasssword = bcrpyt.hashSync(password , 10);
     const newUser = new User({username, email, password:hashedPasssword })
@@ -16,4 +17,21 @@ const signup = async (req,res,next)=>{
 
 };
 
-export default signup;
+export const signin = async (req,res,next) =>{
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(404,"User not Found"))
+        const validPassword = bcrpyt.compareSync(password,validUser.password);
+        if (!validPassword) return next(errorHandler(401,"Wrong credentials!!!"));
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const {password:pass, ...rest} = validUser._doc
+        res.cookie('access_token',token,{ httpOnly: true})
+        .status(200).json(rest);
+        //if wanna limit cookie time then add {new Date(Date.now() +24*60*60*1000)} rather than being a session
+        
+    }catch(error){
+        next(error);
+    }
+}
+
